102de69c7cf7ec6eb8cee25e344ecff95706f4f4
정현수 <junghss0113@gmail.com>
2023-11-13 23:26:10 +0900

    Test: 메뉴를 저장하는 열거체에 대한 테스트 코드 구현

---
+++ src/test/java/christmas/domain/MenuTest.java
@@ -0,0 +1,21 @@
+package christmas.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+class MenuTest {
+
+    @DisplayName("정확한 메뉴 이름이 주어진 경우 메뉴 객체를 캐싱하여 반환한다.")
+    @Test
+    void 인스턴스_반환_테스트() {
+        String menuName = Arrays.stream(Menu.values())
+                .findAny()
+                .orElseThrow()
+                .getMenuName();
+
+        Assertions.assertEquals(menuName, Menu.from(menuName).getMenuName());
+    }
+}
