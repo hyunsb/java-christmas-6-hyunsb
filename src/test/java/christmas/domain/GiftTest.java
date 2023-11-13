a79c0e3b51ed126ed02f3a219eb1bc1126fc402f
정현수 <junghss0113@gmail.com>
2023-11-13 16:01:15 +0900

    Test: 증정 메뉴 선택 테스트 코드 구현

---
+++ src/test/java/christmas/domain/GiftTest.java
@@ -0,0 +1,32 @@
+package christmas.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Optional;
+
+class GiftTest {
+
+    @DisplayName("총 주문 금액이 12만원 이상인 경우 증정 메뉴를 반환한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {120000, 1300000})
+    void 증정_메뉴_반환_테스트(int totalAmount) {
+        Optional<Gift> gift = Gift.select(totalAmount);
+
+        Assertions.assertAll(
+                () -> Assertions.assertTrue(gift.isPresent()),
+                () -> Assertions.assertEquals(Gift.MENU, gift.get())
+        );
+    }
+
+    @DisplayName("총 주문 금액이 12만원 미만인 경우 비어있는 Optional을 반환한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {12000, 110000})
+    void 증정_메뉴_없음_테스트(int totalAmount) {
+        Optional<Gift> gift = Gift.select(totalAmount);
+
+        Assertions.assertTrue(gift.isEmpty());
+    }
+}
