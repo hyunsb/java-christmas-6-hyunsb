0e14cd03ebbaf633f6ffac5f111579bd40c21fc9
정현수 <junghss0113@gmail.com>
2023-11-13 14:25:56 +0900

    Test: 추가된 검증에 따른 테스트 코드 구현

--- src/test/java/christmas/domain/OrdersTest.java
+++ src/test/java/christmas/domain/OrdersTest.java
@@ -14,12 +14,17 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 class OrdersTest {
 
     private List<Order> pickOrders(int count) {
-        Set<Order> orders = new HashSet<>();
-        while (orders.size() < count) {
+        Set<Order> orderDuplicateCheck = new HashSet<>();
+        while (orderDuplicateCheck.size() < count) {
             Order order = Order.from(pickRandomMenuName(), pickRandomInteger());
-            orders.add(order);
+            orderDuplicateCheck.add(order);
         }
-        return orders.stream().toList();
+
+        List<Order> orders = orderDuplicateCheck.stream().toList();
+        if (orders.size() == this.getBeverageCount(orders)) {
+            return pickOrders(count);
+        }
+        return orders;
     }
 
     private String pickRandomMenuName() {
@@ -30,7 +35,14 @@ class OrdersTest {
 
     private int pickRandomInteger() {
         Random random = new Random();
-        return random.nextInt(1, 200);
+        return random.nextInt(1, 5);
+    }
+
+    private int getBeverageCount(List<Order> orders) {
+        return orders.stream()
+                .filter(Order::isBeverageOrder)
+                .toList()
+                .size();
     }
 
     @DisplayName("중복되지 않는 메뉴리스트가 주어진 경우 인스턴스를 생성한다.")
@@ -56,5 +68,47 @@ class OrdersTest {
         Assertions.assertThatThrownBy(() -> new Orders(orders))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessage.ORDERS_IS_NOT_INVALID.message());
+    }
+
+    @DisplayName("빈 리스트가 주어진 경우 예외를 발생한다.")
+    @Test
+    void 빈_리스트_검증_테스트() {
+        Assertions.assertThatThrownBy(() -> new Orders(List.of()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.ORDERS_IS_NOT_INVALID.message());
+    }
+
+    @DisplayName("음료 메뉴만 주어진 경우 예외를 발생한다.")
+    @Test
+    void 음료_리스트_검증_테스트() {
+        String menuName = this.pickRandomBeverageMenuName();
+        List<Order> orders = List.of(Order.from(menuName, pickRandomInteger()));
+
+        Assertions.assertThatThrownBy(() -> new Orders(orders))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.ORDERS_IS_NOT_INVALID.message());
+    }
+
+    @DisplayName("총 주문 개수가 20개를 초과하는 경우 예외를 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {21, 30})
+    void 주문개수_검증_테스트(int count) {
+        String menuName = Arrays.stream(Menu.values())
+                .filter(menu -> !menu.isBeverage())
+                .findAny()
+                .orElseThrow()
+                .getMenuName();
+
+        Assertions.assertThatThrownBy(() -> new Orders(List.of(Order.from(menuName, count))))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.ORDERS_IS_NOT_INVALID.message());
+    }
+
+    private String pickRandomBeverageMenuName() {
+        return Arrays.stream(Menu.values())
+                .filter(Menu::isBeverage)
+                .findAny()
+                .orElseThrow(IllegalArgumentException::new)
+                .getMenuName();
     }
 }
